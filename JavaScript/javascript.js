d7d711c48aafb7f2bbf44507637712f56ec6e38b
slthurman01 <slthurman01@gmail.com>
2022-10-19 08:29:12 -0500

    Adds files

---
+++ JavaScript/javascript.js
@@ -0,0 +1,21 @@
+const timeBlock9 = document.getElementById("timeBlock9");
+const timeBlock10 = document.getElementById("timeBlock10");
+const timeBlock11 = document.getElementById("timeBlock11");
+const timeBlock12 = document.getElementById("timeBlock12");
+const timeBlock13 = document.getElementById("timeBlock13");
+const timeBlock14 = document.getElementById("timeBlock14");
+const timeBlock15 = document.getElementById("timeBlock15");
+const timeBlock16 = document.getElementById("timeBlock16");
+const timeBlock17 = document.getElementById("timeBlock17");
+
+timeBlock9.innerHTML = "9:00 AM";
+timeBlock10.innerHTML = "10:00 AM";
+timeBlock11.innerHTML = "11:00 AM";
+timeBlock12.innerHTML = "12:00 AM";
+timeBlock13.innerHTML = "1:00 PM";
+timeBlock14.innerHTML = "2:00 PM";
+timeBlock15.innerHTML = "3:00 PM";
+timeBlock16.innerHTML = "4:00 PM";
+timeBlock17.innerHTML = "5:00 PM";
+
+var description = document.getElementsByClassName("description");
--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# module-5-challenge
+# Project Title
+
+Module 5 Challenge
+
+## Description
+
 In this coding challenge, students must create a daily planner that can allow users to save events for each hour of the day.
+
+## Screenshot
+
+<img src="/Users/skipperthurman/bootcamp/module-5-challenge/module-5-challenge/images/screenshot.png">
+
+## Links
+
+<a href="https://skip-thurm.github.io/work-scheduler/">Link to Deployed Page</a>
+
+<a href="https://github.com/skip-thurm/work-scheduler">Link to Github Page with Code</a>
+
+## Author
+
+Skipper Thurman
+
+email: slthurman01@gmail.com
+
+<a href="https://github.com/skip-thurm">Personal Github Page</a>
